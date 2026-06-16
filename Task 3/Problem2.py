c7b3a5e547b9796df298c8f4b4d83824e33f16c8
Marjia275 <140470009+Marjia275@users.noreply.github.com>
2026-06-16 16:14:27 +0600

    Update Problem2.py

--- Task 3/Problem2.py
+++ Task 3/Problem2.py
@@ -1,1 +1,15 @@
+pattern=input()
+text=input()
+d=(int(input()))
 
+
+for i in range (len(text)-len(pattern)):
+  c=0
+
+  for j in range (len(pattern)):
+    if pattern[j]!=text[i+j]:
+      c+=1
+
+  if(c<=d):
+    print(i,end=" ")    
+    
